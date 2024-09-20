db3555e9f3ac7d3e63fe40d8dcbad256a9a5a1c7
Bruno Perez <brunoperez24793@gmail.com>
2024-09-20 17:27:55 -0300

    adding post of food

--- obj/Debug/net8.0/food-rating.AssemblyInfo.cs
+++ obj/Debug/net8.0/food-rating.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("food-rating")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d226e82504f4acb823616c11f3ea8be18959dbe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("food-rating")]
 [assembly: System.Reflection.AssemblyTitleAttribute("food-rating")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
